fc000326be84494506b4e1108df63e0d5492ec46
zcip <zcip.web@gmail.com>
2023-04-13 17:07:14 +0900

    remove jsx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
-import { defineConfig } from 'astro/config';
-
-import preact from "@astrojs/preact";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [preact()]
-});
+export default defineConfig({});
