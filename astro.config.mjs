81d31500e4f5a3b378fed2144ba68795416a4607
zcip <zcip.web@gmail.com>
2023-04-14 19:28:13 +0900

    add tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://zcip-reading-list.netlify.app",
+  integrations: [tailwind()],
+});
