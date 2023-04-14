38bb8f8ff73db98d8b81489693922d21a667b455
zcip <zcip.web@gmail.com>
2023-04-14 19:27:56 +0900

    update rss config

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,11 +1,21 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import type { RSSOptions } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
-export async function get() {
+export async function get(context: RSSOptions) {
+  const blog = await getCollection("posts");
+  const filterdBlog = blog.filter((post) => !post.data.isDraft);
+
   return rss({
-    title: "Astro Learner | Blog",
-    description: "My journery learning Astro",
-    site: "https://zcip-reading-list.netlify.app",
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    title: "zcip's reading list",
+    description: "zcipが興味を持ったコンテンツをだいたい毎週まとめます",
+    site: context.site,
+    items: filterdBlog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}`,
+    })),
     customData: `<language>ja</language>`,
   });
 }
