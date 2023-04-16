3b015c20af7a3d2a57ce29e806e20e3a790b743e
zcip <zcip.web@gmail.com>
2023-04-17 01:50:53 +0900

    Add create script `npm run new`

---
+++ scripts/create-content.mjs
@@ -0,0 +1,27 @@
+import fs from "node:fs/promises";
+import { format } from "date-fns";
+
+const dateStr = format(new Date(), "yyyy-MM-dd");
+
+const template = `
+---
+isDraft: true
+title: TODO
+slug: "${dateStr}"
+description: TODO
+pubDate: ${dateStr}
+---
+
+## 読んだ記事
+
+
+## その他
+
+`;
+
+fs.writeFile(`src/content/posts/${dateStr}.md`, template, {
+  encoding: "utf-8",
+  flag: "wx", // 存在する場合上書きしない
+}).catch((error) => {
+  console.log(error.message);
+});
