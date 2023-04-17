1088f2503bc1cfaddf0e6b09a1cd8fcfb15dafc8
zcip <zcip.web@gmail.com>
2023-04-18 01:23:55 +0900

    次へと前へを実装

---
+++ src/lib/getPrevAndNext.ts
@@ -0,0 +1,19 @@
+import type { AstroGlobal } from "astro";
+import { getCollection } from "astro:content";
+
+export async function getPrevAndNext(Astro: Readonly<AstroGlobal>) {
+  const posts = await getCollection("posts");
+  const links = posts.map((post) => ({
+    text: post.data.title,
+    pubDate: post.data.pubDate,
+    link: post.slug,
+  }));
+
+  const index = links.findIndex((x) => Astro.url.pathname.endsWith(x.link));
+
+  return {
+    previous: index > 0 ? links[index - 1] : undefined,
+    next:
+      index !== -1 && index < links.length - 1 ? links[index + 1] : undefined,
+  };
+}
