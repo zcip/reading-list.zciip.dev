3f09df9857dfc63f2c7f630ed2780166852ee01a
zcip <zcip.web@gmail.com>
2023-04-18 15:43:57 +0900

    リストを新着順でソート

--- src/lib/getPublishedPosts.ts
+++ src/lib/getPublishedPosts.ts
@@ -1,11 +1,15 @@
 import { getCollection } from "astro:content";
+import { isAfter } from "date-fns";
 
 /**
  * Prod環境かつisDraftがfalseの公開済みの投稿を取得
+ * 結果は新着公開順で整列
  */
 export async function getPublishedPosts() {
   const posts = await getCollection("posts");
-  return posts.filter((post) => {
-    return !(post.data.isDraft && import.meta.env.PROD);
-  });
+  return posts
+    .filter((post) => {
+      return !(post.data.isDraft && import.meta.env.PROD);
+    })
+    .sort((a, b) => (isAfter(b.data.pubDate, a.data.pubDate) ? 1 : -1));
 }
