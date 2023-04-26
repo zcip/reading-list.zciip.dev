8634bf31bf315aa19762e4a76eaae2169735917d
zcip <zcip.web@gmail.com>
2023-04-27 01:44:39 +0900

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://reading-list.zcip.dev/",
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    partytown({ config: { forward: ["dataLayer.push"] } }),
+  ],
   trailingSlash: "always",
   markdown: {
     shikiConfig: {
