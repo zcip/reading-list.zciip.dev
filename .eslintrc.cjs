d69801878b659056c805f92ca6b86e993f66b4b7
zcip <zcip.web@gmail.com>
2023-04-27 15:01:39 +0900

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,41 @@
+/**
+ * @see https://github.com/withastro/docs/blob/3c2d14720ae5e5aeec102c755e8f07f654b8f6c1/.eslintrc.js
+ * @type {import("eslint").ESLint.ConfigData}
+ */
 module.exports = {
   extends: ["plugin:astro/recommended", "plugin:astro/jsx-a11y-recommended"],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
+    {
+      files: ["*.ts"],
+      parser: "@typescript-eslint/parser",
+      extends: ["plugin:@typescript-eslint/recommended"],
+      rules: {
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
+        ],
+        "@typescript-eslint/no-non-null-assertion": "off",
+      },
+    },
+    {
+      files: ["*.d.ts"],
+      rules: {
+        "@typescript-eslint/triple-slash-reference": "off",
+      },
+    },
+    {
+      // Define the configuration for `<script>` tag.
+      // Script in `<script>` is assigned a virtual file name with the `.js` extension.
+      files: ["**/*.astro/*.js", "*.astro/*.js"],
+      parser: "@typescript-eslint/parser",
+    },
+  ],
 };
